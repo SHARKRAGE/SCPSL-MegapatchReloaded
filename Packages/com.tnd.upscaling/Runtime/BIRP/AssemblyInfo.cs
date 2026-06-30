e39b7f0445c2042cdc2798dc026de13384765e86
SHARKHACK <kevinsamfox4014@gmail.com>
2026-06-30 13:59:30 +0400

    deleted issue package

--- Packages/com.tnd.upscaling/Runtime/BIRP/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("TND.Upscaling.Framework.MakeInternalsVisible")]
-[assembly: InternalsVisibleTo("TND.Upscaling.Framework.Editor.BIRP")]
-[assembly: InternalsVisibleTo("Unity.Postprocessing.Runtime")]
